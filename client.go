f26b05633c74640a3d16f262c685be24918bbdd1
Bill Glover <bill@billglover.co.uk>
2019-03-26 22:10:26 +0000

    Create basic client

---
+++ client.go
@@ -0,0 +1,134 @@
+package transferwise
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+
+	"github.com/pkg/errors"
+)
+
+const (
+	liveURL    = "https://api.transferwise.com"
+	sandboxURL = "https://api.sandbox.transferwise.tech"
+	userAgent  = "github.com/billglover/transferwise"
+)
+
+// Client is the TransferWise client
+type Client struct {
+	baseURL   *url.URL
+	userAgent string
+	client    *http.Client
+}
+
+// NewClient returns a new Starling API client. If a nil httpClient is
+// provided, http.DefaultClient will be used. To use API methods which require
+// authentication, provide an http.Client that will perform the authentication
+// e.g. the golang.org/x/oauth2 library.
+func NewClient(c *http.Client) *Client {
+	if c == nil {
+		c = http.DefaultClient
+	}
+
+	baseURL, _ := url.Parse(sandboxURL)
+
+	return &Client{baseURL: baseURL, userAgent: userAgent, client: c}
+}
+
+// ClientOptions provides a set of options that can be used to configure the Client.
+type ClientOptions struct {
+	baseURL   *url.URL
+	userAgent string
+}
+
+// NewClientWithOptions takes ClientOptions, configures and returns a new client.
+func NewClientWithOptions(c *http.Client, opts ClientOptions) *Client {
+	client := NewClient(c)
+
+	client.baseURL = opts.baseURL
+	client.userAgent = opts.userAgent
+
+	return client
+}
+
+// NewRequest takes a method, path and optional body. It returns an *http.Request.
+// If a non-nil body is provided it will be JSON encoded and included in the request.
+func (c *Client) NewRequest(method, path string, body interface{}) (*http.Request, error) {
+	u, err := c.baseURL.Parse(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var buf io.ReadWriter
+	if body != nil {
+		buf = new(bytes.Buffer)
+		enc := json.NewEncoder(buf)
+		enc.SetEscapeHTML(false)
+		err := enc.Encode(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	req, err := http.NewRequest(method, u.String(), buf)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Accept", "application/json")
+
+	if body != nil {
+		req.Header.Set("Content-Type", "application/json")
+	}
+
+	if c.userAgent != "" {
+		req.Header.Set("User-Agent", c.userAgent)
+	}
+
+	return req, nil
+}
+
+// Do sends a request to the API and returns the response. An error is returned
+// if the request cannot be sent or if the API returns an error. If a response is
+// received, the body is decoded and stored in the value pointed to by v.
+func (c *Client) Do(ctx context.Context, req *http.Request, v interface{}) (*http.Response, error) {
+	req = req.WithContext(ctx)
+	resp, err := c.client.Do(req)
+
+	if err != nil {
+		select {
+		case <-ctx.Done():
+			return nil, errors.Wrap(err, ctx.Err().Error())
+		default:
+			return nil, err
+		}
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to read body")
+	}
+
+	err = resp.Body.Close()
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to close body")
+	}
+
+	if v != nil && len(data) != 0 {
+		err = json.Unmarshal(data, v)
+
+		switch err {
+		case nil:
+		case io.EOF:
+			err = nil
+		default:
+			err = errors.Wrap(err, "unable to parse API response")
+		}
+	}
+
+	return resp, err
+}
---
+++ client_test.go
@@ -0,0 +1,164 @@
+package transferwise
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+// TestNewClient confirms that a client can be created with the default baseURL
+// and default User-Agent.
+func TestNewClient(t *testing.T) {
+	c := NewClient(nil)
+
+	if got, want := c.baseURL.String(), sandboxURL; got != want {
+		t.Errorf("client should use default url: got %s, want %s", got, want)
+	}
+
+	if got, want := c.userAgent, userAgent; got != want {
+		t.Errorf("client should use default user-agent: got %s, want %s", got, want)
+	}
+}
+
+// TestNewClientWithOptions confirms that a new client can be created by passing
+// in custom ClientOptions.
+func TestNewClientWithOptions(t *testing.T) {
+	baseURL, _ := url.Parse("https://dummyurl:4000")
+	userAgent := "dummy user agent"
+
+	opts := ClientOptions{
+		baseURL:   baseURL,
+		userAgent: userAgent,
+	}
+	c := NewClientWithOptions(nil, opts)
+
+	if got, want := c.baseURL.String(), baseURL.String(); got != want {
+		t.Errorf("client should use custom url: got %s, want %s", got, want)
+	}
+
+	if got, want := c.userAgent, userAgent; got != want {
+		t.Errorf("client should use custom user-agent: got %s, want %s", got, want)
+	}
+}
+
+// TestNewRequest confirms that new client requests are created with the
+// correct path, userAgent and body.
+func TestNewRequest(t *testing.T) {
+	client := NewClient(nil)
+	method := http.MethodGet
+	path := "/test"
+	body := struct {
+		TestName    string `json:"name"`
+		PackageName string `json:"pkg"`
+	}{
+		TestName:    "TestNewRequest",
+		PackageName: "github.com/billglover/transferwise",
+	}
+	bodyJSON := `{"name":"TestNewRequest","pkg":"github.com/billglover/transferwise"}`
+
+	t.Run("invalid path", func(st *testing.T) {
+		_, err := client.NewRequest(method, path+string(byte(0x00)), body)
+		if err == nil {
+			st.Error("expected an error: got none")
+		}
+	})
+
+	t.Run("invalid body", func(st *testing.T) {
+		body := struct {
+			Func func() error
+		}{
+			Func: func() error { return nil },
+		}
+		_, err := client.NewRequest(method, path, body)
+		if err == nil {
+			st.Error("expected an error: got none")
+		}
+	})
+
+	t.Run("invalid method", func(st *testing.T) {
+		_, err := client.NewRequest(string(byte(0x00)), path, body)
+		if err == nil {
+			st.Error("expected an error: got none")
+		}
+	})
+
+	t.Run("valid request", func(st *testing.T) {
+		req, err := client.NewRequest(method, path, body)
+
+		if err != nil {
+			st.Fatalf("unexpected error: got %v", err)
+		}
+
+		if got, want := req.URL.String(), sandboxURL+path; got != want {
+			st.Errorf("invalid path: got %s, want %s", got, want)
+		}
+
+		if got, want := req.UserAgent(), userAgent; got != want {
+			st.Errorf("invalid user-agent: got %s, want %s", got, want)
+		}
+
+		b, err := ioutil.ReadAll(req.Body)
+		defer req.Body.Close()
+		if err != nil {
+			st.Fatalf("unexpected error: got %v", err)
+		}
+
+		if got, want := strings.TrimSpace(string(b)), bodyJSON; got != want {
+			st.Errorf("unexpected body:\n\tgot %s\n\twant %s", got, want)
+		}
+	})
+}
+
+func TestDo(t *testing.T) {
+
+	t.Run("successful POST request", func(st *testing.T) {
+
+		client, mux, _, teardown := setup()
+		defer teardown()
+
+		method := http.MethodPost
+
+		type foo struct{ A string }
+
+		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			if got, want := r.Method, method; got != want {
+				st.Errorf("unexpected method: got %s, want %s", got, want)
+			}
+			fmt.Fprint(w, `{"A":"a"}`)
+		})
+
+		want := &foo{"a"}
+		got := new(foo)
+
+		req, _ := client.NewRequest(method, "", nil)
+		_, err := client.Do(context.Background(), req, got)
+		if err != nil {
+			st.Fatalf("unexpected error: got %v", err)
+		}
+
+		if !reflect.DeepEqual(got, want) {
+			st.Error("unexpected response body")
+		}
+	})
+
+}
+
+// Setup establishes a test Server that can be used to provide mock responses during testing.
+// It returns a pointer to a client, a mux, the server URL and a teardown function that
+// must be called when testing is complete.
+func setup() (*Client, *http.ServeMux, string, func()) {
+	mux := http.NewServeMux()
+	server := httptest.NewServer(mux)
+
+	client := NewClient(nil)
+	url, _ := url.Parse(server.URL + "/")
+	client.baseURL = url
+
+	return client, mux, server.URL, server.Close
+}
